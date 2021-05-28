7f8321636e536373c785ef7a38cd13727baef1e4
Dominik <Dominik@192.168.33.9>
2021-05-28 16:41:25 +0200

    first set-up

---
+++ main.m
@@ -0,0 +1,12 @@
+%% Becherwurf
+
+% Parameter
+
+par.m_Ball = 2.7e-3;   % Masse des Balls in kg
+par.d_Ball = 0.04;     % Durchmesser des Balls in m
+par.d_Becher = 0.095;  % Durchmesser des Bechers in m
+par.s_Becher = 5e-3;   % Abstand der Becher in m
+par.g = 9.81;          % Fallbeschleunigung in m/s²
+par.l = 2.1;           % Länge des Tisches in m
+par.cw = 0.47;         % Luftwiderstandsbeiwert der Kugel
+par.rho = 1.2;         % Dichte der Luft in kg/m³
